585dfc8bb285f0f86f51bc9652c74b346a880b99
Darion Welch <darion.welch@gmail.com>
2017-02-09 19:51:52 -0600

    more stuff

--- convexhull.js
+++ convexhull.js
@@ -12,8 +12,8 @@ document.addEventListener('DOMContentLoaded', () => {
     let plots = [];
 
     for (let i = 0; i < num; i++) {
-      let x = Math.floor(Math.random() * boundingBox.width);
-      let y = Math.floor(Math.random() * boundingBox.height);
+      let x = Math.random() * boundingBox.width;
+      let y = Math.random() * boundingBox.height;
       plots.push(plot(x, y));
     }
 
@@ -47,42 +47,44 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const orderedPlots = plots
       .sort((a, b) => {
-        return getCoordinatePair(a).x - getCoordinatePair(b).x
+        return getCoordinatePair(a).x - getCoordinatePair(b).x;
       });
 
-    function makesRightTurn(plots) {
-      if (plots.length !== 3) return false;
+    function makesRightTurn(plots, reverse) {
+      const p = getCoordinatePair(plots[0]);
+      const q = getCoordinatePair(plots[1]);
+      const r = getCoordinatePair(plots[2]);
 
-      const plot0 = getCoordinatePair(plots[0]);
-      const plot1 = getCoordinatePair(plots[1]);
-      const plot2 = getCoordinatePair(plots[2]);
+      const value = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
 
-      if (plot1.x > plot0.x || plot1.x > plot2.x) {
+      if (value == 2) {
         return true;
       }
 
       return false;
     }
 
+    // Put the points p[0] and p[1] in a list UpperHull, with p[0] at index 0
     UpperHull.push(orderedPlots[0]);
     UpperHull.push(orderedPlots[1]);
 
-    // Put the points p[0] and p[1] in a list UpperHull, with p[0] at index 0
     // for: i = 2, i < n, i++
-      // do: append p[i] to UpperHull
-        // while: UpperHull contains more than 2 points and the last 3 points
-        //        in UpperHull do not make a right turn
-          // do: Delete the middle of the last three points from UpperHull
-
     for (let i = 2; i < orderedPlots.length; i++) {
+      // do: append p[i] to UpperHull
       UpperHull.push(orderedPlots[i]);
 
-      while(UpperHull.length > 2 && !makesRightTurn(UpperHull.slice(UpperHull.length - 3, UpperHull.length))) {
-        UpperHull.splice(UpperHull.length - 2);
+      // while: UpperHull contains more than 2 points and the last 3 points
+      //        in UpperHull do not make a right turn
+      
+      while(UpperHull.length > 2 && !makesRightTurn(UpperHull.slice(UpperHull.length - 3))) {
+        // do: Delete the middle of the last three points from UpperHull
+        UpperHull.splice(UpperHull.length - 2, 1);
       }
     }
 
-    console.log(UpperHull);
+    UpperHull.forEach(plot => {
+      plot.style.fill = 'red';
+    });
     
     // Put the points p[n] and p[n-1]  in LowerHull with p[n] at index 0
     // for: i = n - 2, i > 1, i--
@@ -90,6 +92,22 @@ document.addEventListener('DOMContentLoaded', () => {
         // while: LowerHull contains more than 2 points and the last three points
         //        in LowerHull do not make a right turn
           // do: Delete the middle of the last three points from LowerHull
+          
+    LowerHull.push(orderedPlots[orderedPlots.length - 1]);
+    LowerHull.push(orderedPlots[orderedPlots.length - 2]);
+
+    for (let j = orderedPlots.length - 3; j > 0; j--) {
+      LowerHull.push(orderedPlots[j]);
+
+      while(LowerHull.length > 2 && !makesRightTurn(LowerHull.slice(LowerHull.length - 3), true)) {
+        // do: Delete the middle of the last three points from LowerHull
+        LowerHull.splice(LowerHull.length - 2, 1);
+      }
+    }
+
+    LowerHull.forEach(plot => {
+      plot.style.fill = 'red';
+    });
     // Remove the first and the last point from LowerHull to avoid duplication of points
     // Append Lowerfull to UpperHull and call the resulting list L
     // return L
@@ -99,8 +117,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const svgNS = 'http://www.w3.org/2000/svg';
 
   const boundingBox = {
-    height: 500,
-    width: 500
+    height: 300,
+    width: 300
   };
 
   svgEl.style.height = `${boundingBox.height}px`;
